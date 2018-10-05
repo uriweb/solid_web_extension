d755121de1b4dcc217ec30213f8174d954fdeb53
John Pennypacker <jpennypacker@uri.edu>
2018-10-05 09:37:27 -0400

    light clean up

--- content.js
+++ content.js
@@ -1,56 +1,48 @@
+/**
+ * set us up some words to updgrade
+ */
 var solidAdjectives = ["strong", "best", "big", "great", "good", "major", "distinctive", "active"];
 var exquisiteAdjectives = ["beautiful", "wonderful", "delightful", "exciting", "detailed"];
-var weakAdjectives = ["dumb", "lame", "poor", "dull", "lackluster"];
-
-// var r = new RegExp("\\b" + adjectives.join("|") + "\\b", "gi");
-// "That's big thinking.  Big. Real BIG!".match(r)
-
-var elements = document.getElementsByTagName('*');
-
-for (var i = 0; i < elements.length; i++) {
-	var element = elements[i];
-
-	for (var j = 0; j < element.childNodes.length; j++) {
-		var node = element.childNodes[j];
-
-		if (node.nodeType === 3) {
-			var text, r, replacedText;
-			text = node.nodeValue;
-			
-			//@todo: refactor
-			//@todo: handle comparative and superlative
-			
-			// solidify
-			r = new RegExp("\\b(" + solidAdjectives.join("|") + ")\\b", "gi");
-			replacedText = text.replace(r, function(match, r1, offset, string) {
-				var replacement = "solid";
-				return preserveCase(r1, offset, string, replacement);
-			});
-
-			// exquisinate
-			r = new RegExp("\\b(" + exquisiteAdjectives.join("|") + ")\\b", "gi");
-			replacedText = replacedText.replace(r, function(match, r1, offset, string) {
-				var replacement = "exquisite";
-				return preserveCase(r1, offset, string, replacement);
-			});
-
-			// weaken
-			r = new RegExp("\\b(" + exquisiteAdjectives.join("|") + ")\\b", "gi");
-			replacedText = replacedText.replace(r, function(match, r1, offset, string) {
-				var replacement = "weak";
-				return preserveCase(r1, offset, string, replacement);
-			});
+var weakAdjectives = ["small", "tiny", "dumb", "lame", "poor", "dull", "lackluster"];
 
 
-			if (replacedText !== text) {
-				element.replaceChild(document.createTextNode(replacedText), node);
-			}
-			
-		}
-	}
+/**
+ * Do the replacements
+ * @todo: refactor into more sensible chunks
+ * @todo: handle comparative and superlative cases
+ *
+ */
+function replaceWords(text) {
+	
+	// solidify
+	r = new RegExp("\\b(" + solidAdjectives.join("|") + ")\\b", "gi");
+	replacedText = text.replace(r, function(match, r1, offset, string) {
+		var replacement = "solid";
+		return preserveCase(r1, offset, string, replacement);
+	});
+
+	// exquisinate
+	r = new RegExp("\\b(" + exquisiteAdjectives.join("|") + ")\\b", "gi");
+	replacedText = replacedText.replace(r, function(match, r1, offset, string) {
+		var replacement = "exquisite";
+		return preserveCase(r1, offset, string, replacement);
+	});
+
+	// weaken
+	r = new RegExp("\\b(" + exquisiteAdjectives.join("|") + ")\\b", "gi");
+	replacedText = replacedText.replace(r, function(match, r1, offset, string) {
+		var replacement = "weak";
+		return preserveCase(r1, offset, string, replacement);
+	});
 }
 
-
+/**
+ * Check the case of the matched word and try to match it with the replacement
+ * Right now, it basically checks if the word starts with a cap, if so,
+ * it capitalizes the replacement.
+ * after that, if the last letter is a capital, it returns the replacement in all caps
+ * only works for ASCII caps.
+ */
 function preserveCase(match, offset, string, replacement) {
 
 	// check if the first letter is capitalized
@@ -67,4 +59,34 @@ function preserveCase(match, offset, string, replacement) {
 	
 	
 	return replacement;
+}
+
+
+
+
+
+
+/** 
+ * go through the DOM tree, identify text to replace
+ */
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+	var element = elements[i];
+
+	for (var j = 0; j < element.childNodes.length; j++) {
+		var node = element.childNodes[j];
+
+		if (node.nodeType === 3) {
+			var text, r, replacedText;
+			
+			// do replacements
+			text = replaceWords(node.nodeValue);
+
+			if (replacedText !== text) {
+				element.replaceChild(document.createTextNode(replacedText), node);
+			}
+			
+		}
+	}
 }
